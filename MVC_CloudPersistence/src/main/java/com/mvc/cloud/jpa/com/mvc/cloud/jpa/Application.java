def41a18e97e2411cf8ca82b55636fa9879de00f
Diego Maŕquez <desarrollo.marquez@gmail.com>
2019-02-15 00:44:30 -0500

    Adicion nuevo proyecto MVC

---
+++ MVC_CloudPersistence/src/main/java/com/mvc/cloud/jpa/com/mvc/cloud/jpa/Application.java
@@ -0,0 +1,14 @@
+package com.mvc.cloud.jpa.com.mvc.cloud.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
