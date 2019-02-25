04cee6c1123e0caf8e262903be63c154f2cca2ba
Diego Maŕquez <desarrollo.marquez@gmail.com>
2019-02-24 22:07:12 -0500

    Adicion Controlador Tipo

--- API_CloudPersistence/src/main/java/com/api/cloud/jpa/repository/TipoRepository.java
+++ API_CloudPersistence/src/main/java/com/api/cloud/jpa/repository/TipoRepository.java
@@ -4,12 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.api.cloud.jpa.model.Auto;
+import com.api.cloud.jpa.model.Tipo;
 
 /**
  * Created by diego on 14/02/19.
  */
 
 @Repository
-public interface TipoRepository extends JpaRepository<Auto, Long> {
+public interface TipoRepository extends JpaRepository<Tipo, Long> {
 
 }
