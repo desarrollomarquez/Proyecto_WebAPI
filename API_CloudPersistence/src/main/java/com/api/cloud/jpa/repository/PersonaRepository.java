24de50b9a0f63bee9ab6edebd0824953028b48bb
Diego Maŕquez <desarrollo.marquez@gmail.com>
2019-03-01 15:35:45 -0500

    Adicion repositorio Persona

---
+++ API_CloudPersistence/src/main/java/com/api/cloud/jpa/repository/PersonaRepository.java
@@ -0,0 +1,15 @@
+package com.api.cloud.jpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.cloud.jpa.model.Auto;
+
+/**
+ * Created by diego on 14/02/19.
+ */
+
+@Repository
+public interface PersonaRepository extends JpaRepository<Auto, Long> {
+
+}
