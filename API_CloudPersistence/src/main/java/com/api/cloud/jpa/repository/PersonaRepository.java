c50b4fcb18fd5091629e92bf2edfc6a5190be7ca
Diego Maŕquez <desarrollo.marquez@gmail.com>
2019-03-01 15:37:28 -0500

    Ajustes repositorio Persona

--- API_CloudPersistence/src/main/java/com/api/cloud/jpa/repository/PersonaRepository.java
+++ API_CloudPersistence/src/main/java/com/api/cloud/jpa/repository/PersonaRepository.java
@@ -3,13 +3,13 @@ package com.api.cloud.jpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.api.cloud.jpa.model.Auto;
+import com.api.cloud.jpa.model.Persona;
 
 /**
  * Created by diego on 14/02/19.
  */
 
 @Repository
-public interface PersonaRepository extends JpaRepository<Auto, Long> {
+public interface PersonaRepository extends JpaRepository<Persona, Long> {
 
 }
